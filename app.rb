31c9a6faad2c85c5f747006d8863de9f0982eaa1
Richard Davis <rdavis.bacs@gmail.com>
2014-10-15 18:00:30 -0400

    save mixed audio to tmp

--- app.rb
+++ app.rb
@@ -2,11 +2,12 @@ require 'sinatra'
 
 get '/' do
   @files = Dir["public/*.wav"].map { |file| file.split('/')[1] }
+  @files += Dir["tmp/*.wav"])
   erb :index
 end
 
 get '/mix' do
-  `sox public/a.wav public/c_sharp.wav public/mix.wav &`
+  `sox public/a.wav public/c_sharp.wav tmp/mix.wav &`
   redirect '/'
 end
 
