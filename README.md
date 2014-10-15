4d7f6bd424ef998cf52a2e96f281b575eeafa8ec
Richard Davis <rdavis.bacs@gmail.com>
2014-10-15 17:45:22 -0400

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Audio Demo
+
+Mix some audio on Heroku
+
+
+## Heroku setup
+`heroku config:add BUILDPACK_URL=https://github.com/ddollar/heroku-buildpack-multi.git`
+
+[buildpack documentation](https://blog.heroku.com/archives/2012/7/17/buildpacks)
---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'pry'
+
+get '/' do
+  @files = Dir["public/*.wav"].map { |file| file.split('/')[1] }
+  erb :index
+end
+
+get '/mix' do
+  `sox public/a.wav public/c_sharp.wav public/mix.wav`
+  redirect '/'
+end
+
+get '/remove_mix' do
+  `rm public/mix.wav`
+  redirect '/'
+end
